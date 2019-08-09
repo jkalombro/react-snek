bee91925992bd7e9459232c971237d5bd7b47c08
Jerich Alombro <jerich.alombro@fsoft.com.vn>
2019-08-09 15:55:46 +0800

    Updates

---
+++ precache-manifest.d8edb38e87cbb73de5f0f7e51790dd96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf602b145d1d47f3636965a6cccf6a1",
+    "url": "/react-snek/index.html"
+  },
+  {
+    "revision": "b903909bafccab24d7cf",
+    "url": "/react-snek/static/css/main.93a3ff6f.chunk.css"
+  },
+  {
+    "revision": "c4d19f98f29caeb37c96",
+    "url": "/react-snek/static/js/2.e491e06c.chunk.js"
+  },
+  {
+    "revision": "b903909bafccab24d7cf",
+    "url": "/react-snek/static/js/main.92bc1158.chunk.js"
+  },
+  {
+    "revision": "e037d10deb72bb61cc55",
+    "url": "/react-snek/static/js/runtime~main.aeda6471.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6404e519edcd1f7dfebf833dbe44efa.js"
+  "/react-snek/precache-manifest.d8edb38e87cbb73de5f0f7e51790dd96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-snek/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
