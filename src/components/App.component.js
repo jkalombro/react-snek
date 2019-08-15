52a05afd01b5b3996ec7adf6c5ca8d5d9526a51a
Jerich Alombro <jerich.alombro@fsoft.com.vn>
2019-08-15 16:57:22 +0800

    changed app wrapper to position absolute

--- src/components/App.component.js
+++ src/components/App.component.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
 export const AppWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
   text-align: center;
   background-color: #282c34;
-  min-height: ${props => props.screenmode==="web" ? '100vh': '90.5vh'};
   display: flex;
   flex-direction: ${props => props.screenmode==="web" ? 'row' : 'column'};
   align-items: center;
